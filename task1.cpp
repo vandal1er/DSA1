29c12be2006531ce0abaf895fddd8a15fda09bfe
vandal1er <165736672+vandal1er@users.noreply.github.com>
2024-06-21 13:37:50 +0800

    Update task1.cpp

--- task1.cpp
+++ task1.cpp
@@ -1,1 +1,38 @@
+#include <iostream>
 
+using namespace std;
+
+int findMax(int* arr, int size)
+{
+	long max = -99999999999;
+	
+	for(int i = 0; i < size; i++)
+	{
+		if (arr[i] > max)
+		{
+			max = arr[i];
+		}
+	}
+	
+	return max;
+}
+
+int main()
+{
+	int arraySize;
+	cout << "Enter size of array: ";
+	cin >> arraySize;
+	
+	int array[arraySize];
+	
+	for(int i = 0; i < arraySize; i++)
+	{
+		cout << "Enter element " << i + 1 << ": ";
+		cin >> array[i];
+	}
+	
+	int maxElement = findMax(array, arraySize);
+	cout << "Maximum element: " << maxElement;
+	
+	return 0;
+}
